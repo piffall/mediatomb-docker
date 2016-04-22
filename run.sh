d4a9e36c8f6b875260ab7dd98fce0a654f47d3d6
Cristòfol Torrens <piffall@gmail.com>
2016-04-23 00:24:47 +0200

    Some errors fixed

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker run --name="mediatomb" --rm --net="host" \
-    -v ~/Links:/media/Links \ # I symlink all media in a directory
-    -v /mnt:/mnt \            # linking to many mount mounts inside mnt
+    -v ~/Links:/media/Links \
+    -v /mnt:/mnt \
     -p 50500:50500 \
     -t piffall/mediatomb
